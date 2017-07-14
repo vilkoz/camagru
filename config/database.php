7172900a34fbbec4a9ae17016aad9aa9c37391b6
vilkoz <vilko.a@gmail.com>
2017-07-14 13:18:54 +0300

    heroku cleardb creds

--- config/database.php
+++ config/database.php
@@ -1,7 +1,22 @@
 <?php
-$DB_DSN = 'mysql:dbname=camagru;host=localhost';
-$DB_USER = 'vilkoz';
-/* $DB_PASS = 'goldfish12'; */
-// $DB_PASS = 'suck my fuck';
-$DB_PASS = '';
+
+if ($_SERVER['SERVER_NAME'] == 'camagru-vilkoz.herokuapp.com')
+{
+	$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$host = $url['host'];
+	$user = $url['user'];
+	$pass = $url['pass'];
+	$db = substr($url['path'], 1);
+	$DB_DSN = 'mysql:dbname=' . $db .';host=' . $host;
+	$DB_USER = $user;
+	$DB_PASS = $pass;
+}
+else
+{
+	$DB_DSN = 'mysql:dbname=camagru;host=localhost';
+	$DB_USER = 'vilkoz';
+	/* $DB_PASS = 'goldfish12'; */
+	// $DB_PASS = 'suck my fuck';
+	$DB_PASS = '';
+}
  ?>
