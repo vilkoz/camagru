80e0dd2f1ad8f3863c62bb09acf711fc89e90fff
vilkoz <vilko.a@gmail.com>
2017-07-14 13:25:03 +0300

    heroku schema setup

--- config/setup.php
+++ config/setup.php
@@ -1,6 +1,14 @@
 <?php
 include_once 'database.php';
-$pdo = new PDO('mysql:host=127.0.0.1', $DB_USER, $DB_PASS);
+try
+{
+	$pdo = new PDO($DB_DSN, $DB_USER, $DB_PASS);
+}
+catch (PDOException $e)
+{
+	echo "Error ".$e->message;
+	$pdo = new PDO('mysql:host=127.0.0.1', $DB_USER, $DB_PASS);
+}
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $sql = file_get_contents('camagru.sql');
 try 
