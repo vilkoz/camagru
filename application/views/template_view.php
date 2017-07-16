f435ed4c2aab12632abf739e7cd5bb8ec2e9c653
vilkoz <vilko.a@gmail.com>
2017-07-16 14:49:35 +0300

    add floating menu button

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -7,6 +7,13 @@
 	<link rel="stylesheet" type="text/css" href="/css/style.css">
   </head>
   <body>
+	<div id="menu" class="show">
+		<div class="menu-container">
+			<div class="bar"></div>
+			<div class="bar"></div>
+			<div class="bar"></div>
+		</div>
+	</div>
 	<div class="page-wrap">
 	<header>
 		<nav>
@@ -38,6 +45,26 @@ else
 	</header>
 	<?php include 'application/views/'.$content_view; ?>
 	</div>
+<script>
+var toggle_menu = function() {
+	document.querySelector('header').classList.toggle('show');
+	document.querySelector('#menu').classList.toggle('show');
+
+	if ((cover = document.querySelector('.perview-cover')) != null) {
+		cover.parentElement.removeChild(cover);
+	}
+	else {
+		div = document.createElement('div');
+		div.classList.add('perview-cover');
+		div.addEventListener('click', toggle_menu);
+		document.querySelector('.page-wrap').appendChild(div);
+	}
+}
+document.getElementById("menu").addEventListener('click', function() {
+	toggle_menu();
+	console.log("gopa");
+});
+</script>
 	<footer>
 		<p>LC team @ 2017</p>
 	</footer>
